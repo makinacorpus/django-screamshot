f253a5784e70e1c036a852fea388c83d1f201ea6
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-04-18 08:25:10 +0200

    Include package data directive

--- setup.py
+++ setup.py
@@ -7,15 +7,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-screamshot',
-    version='0.1',
+    version='0.1.1',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-screamshot',
     description='Web pages capture using Django & CasperJS',
     long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' + 
                      open(os.path.join(here, 'CHANGES')).read(),
+    license='LPGL, see LICENSE file.',
     install_requires = ['django'],
     packages=find_packages(),
+    include_package_data=True,
     zip_safe=False,
     classifiers  = ['Natural Language :: English',
                     'Operating System :: OS Independent',
