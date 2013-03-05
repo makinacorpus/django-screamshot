c28a7357cb8f2ba0e29a70a63f3e20b5e853e6c3
mozillazg <opensource.mozillazg@gmail.com>
2013-03-05 18:32:51 +0800

    Make the code shorter

--- screamshot/views.py
+++ screamshot/views.py
@@ -24,15 +24,13 @@ def capture(request):
     method = parameters.get('method', request.method)
     selector = parameters.get('selector')
     data = parameters.get('data')
-    width = parameters.get('width', '')
-    height = parameters.get('height', '')
 
     try:
-        width = int(width)
+        width = int(parameters.get('width', ''))
     except ValueError:
         width = None
     try:
-        height = int(height)
+        height = int(parameters.get('height', ''))
     except ValueError:
         height = None
 
