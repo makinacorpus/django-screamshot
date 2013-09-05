88a8637ab0ff485a5aec8820f8266eeea6f58d92
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-05 10:54:10 +0200

    Preparing release 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-screamshot',
-    version='0.3.0.dev0',
+    version='0.3.0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-screamshot',
