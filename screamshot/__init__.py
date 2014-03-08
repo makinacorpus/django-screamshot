68ef4d57b61b10f2efed0179c5c7bd3dd3e3cc11
mo-mughrabi <mo.mughrabi@gmail.com>
2014-03-08 15:13:08 +0300

    ref #47 submodule django-streamshot as a tool for screen capture with customization of PHANTOMJS_CMD as configuration

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -6,4 +6,5 @@ app_settings = dict({
     'CAPTURE_ALLOWED_IPS': ('127.0.0.1',),
     'CLI_ARGS': [],
     'CASPERJS_CMD': None,
+    'PHANTOMJS_CMD': None,
 }, **getattr(settings, 'SCREAMSHOT_CONFIG', {}))
