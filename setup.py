e1451c669e9fd800570b6d84b29d46492af91180
J-E Castagnede <j.e.castagnede@gmail.com>
2017-05-05 11:16:39 +0200

    prepare release 0.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-screamshot',
-    version='0.8.0.dev0',
+    version='0.8.0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-screamshot',
